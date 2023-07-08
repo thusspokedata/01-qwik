bff13624ea41042315a90a27ff3a0726933330d2
antoniosaleme <kilo@disroot.org>
2023-07-08 13:42:09 +0200

    signals

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,21 +1,28 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useSignal } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 
-
 export default component$(() => {
+  const pokemonId = useSignal<number>(10); // primitivos, strings booleans
+  // const pokemonStore = useStore() // arrays, objects
   return (
     <>
-<span class="text-5xl">Hola Qwik</span>
+      <span class="text-2xl">Buscador simple</span>
+      <span class="text-9xl">{pokemonId}</span>
+      
+      <div class="mt-2">
+        <button class="btn btn-primary mx-1">Anterior</button>
+        <button class="btn btn-primary mx-1">Siguientes</button>
+      </div>
     </>
   );
 });
 
 export const head: DocumentHead = {
-  title: 'Welcome to Qwik',
+  title: 'PokeQwik',
   meta: [
     {
       name: 'description',
-      content: 'Qwik site description',
+      content: 'My first app using Qwik',
     },
   ],
 };
