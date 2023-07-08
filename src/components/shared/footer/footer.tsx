7a4056a6c87de44dafa9623d007271b6d427289c
antoniosaleme <kilo@disroot.org>
2023-07-08 08:44:38 +0200

    ordering project

--- src/components/shared/footer/footer.tsx
+++ src/components/shared/footer/footer.tsx
@@ -1,9 +1,7 @@
 import { component$ } from '@builder.io/qwik';
-import { useServerTimeLoader } from '~/routes/layout';
 import styles from './footer.module.css';
 
 export default component$(() => {
-  const serverTime = useServerTimeLoader();
 
   return (
     <footer>
@@ -11,7 +9,6 @@ export default component$(() => {
         <a href="https://www.builder.io/" target="_blank" class={styles.anchor}>
           <span>Made with ♡ by Builder.io</span>
           <span class={styles.spacer}>|</span>
-          <span>{serverTime.value.date}</span>
         </a>
       </div>
     </footer>
